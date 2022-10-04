8d5403bfeb5f5f10d8a0d38754239236cf07ed92
mikolajmki <84733024+mikolajmki@users.noreply.github.com>
2022-10-04 22:47:06 +0200

    Included isAuth middleware

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql').graphqlHTTP;
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-
+const isAuth = require('./middleware/is-auth');
 const graphqlSchema = require('./graphql/schema/index');
 const graphqlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+app.use(isAuth);
 
 app.use('/graphql', graphqlHttp({ 
     schema: graphqlSchema,
@@ -19,7 +31,7 @@ app.use('/graphql', graphqlHttp({
 
 mongoose.connect('mongodb+srv://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASSWORD + '@node-rest-shop.7xmmw98.mongodb.net/?retryWrites=true&w=majority')
 .then(() => {
-    app.listen(3000);
+    app.listen(8000);
 })
 .catch(err => {
     console.log(err);
