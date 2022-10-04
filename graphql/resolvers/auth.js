f2a24d8ef43662cadf3fd2249a8957b76ba1600d
mikolajmki <84733024+mikolajmki@users.noreply.github.com>
2022-10-04 22:43:25 +0200

    Added login functionality with jwt and bcrypt

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,3 +1,8 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user');
+const events = require('./events');
+
 module.exports = {
     createUser: async args => {
         try {
@@ -14,6 +19,24 @@ module.exports = {
                 return { ...userSaveResult._doc, password: null };
         } catch (err) {
             throw err;
+        }
+    },
+    login: async ({ email, password }) => {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            throw new Error('User does not exist!');
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            throw new Error('Password is incorrect!');
+        }
+        const token = await jwt.sign({ userId: user.id, email: user.email }, 'somesupersecretkey', {
+            expiresIn: '1h'
+        });
+        return { 
+            userId: user.id,
+            token: token,
+            tokenExpiration: 1
         }
     }
 }
